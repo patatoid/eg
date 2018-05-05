fa9d148214bc26aeadce5c70996db53945be5d15
Leopold Sayous <leosayous@gmail.com>
2018-05-05 20:12:40 +0200

    feat: add response type

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { mainServer } = require('./server');
 
 const creaReacordSave = (socket, name) => {
   socket.on(name, (record) => {
-  const recordToEmit = {deviceName: record.deviceName, index: record.index, hasData: !!record.data};
+  const recordToEmit = {type:'crea', deviceName: record.deviceName, index: record.index, hasData: !!record.data};
     console.log('recordToEmit', recordToEmit)
     SocketService.emitSocketMessage(name, recordToEmit);
     return AdminService.saveCreaRecord(record);
