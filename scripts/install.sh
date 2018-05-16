001cf8bf8c402af30ee2a8b80b794e15c2595404
Leopold Sayous <leosayous@gmail.com>
2018-05-16 21:57:15 +0200

    feat: sudo in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install chromium-browser xserver-xorg xinit matchbox-window-manager --yes
-echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
+sudo echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install libasound2-dev --yes
