3ee47ca7e586e4773b67fa1c26ac2d2c94b374e9
leopold sayous <leosayous@gmail.com>
2018-06-16 08:21:42 +0000

    feat: propage script

--- scripts/propagate_all.sh
+++ scripts/propagate_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-IP='192.168.0.3' sh ./propagate_changes.sh
-IP='192.168.0.4' sh ./propagate_changes.sh
+IP='192.168.0.3' ./propagate_changes.sh
+IP='192.168.0.4' ./propagate_changes.sh
 #IP='192.168.0.5' sh ./propagate_changes.sh
-IP='192.168.0.6' sh ./propagate_changes.sh
+IP='192.168.0.6' ./propagate_changes.sh
 #IP='192.168.0.7' sh ./propagate_changes.sh
